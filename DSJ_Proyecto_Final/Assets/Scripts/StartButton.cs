7aeda0ff3cbc28685bfb1d5a88c6361e912c4947
ChristianIlachoque <cilachoque@unsa.edu.pe>
2023-10-03 23:35:31 -0500

    3 scenes implmented

---
+++ DSJ_Proyecto_Final/Assets/Scripts/StartButton.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public GameObject[] paneles;
+    private int panelActual = 0;
+
+    void Start()
+    {
+        MostrarPanel(panelActual);
+    }
+
+    public void MostrarSiguientePanel()
+    {
+        panelActual++;
+        if (panelActual >= paneles.Length)
+            //panelActual = 0;
+            SceneManager.LoadScene("SampleScene");
+
+        MostrarPanel(panelActual);
+    }
+        
+    private void MostrarPanel(int indice)
+    {
+        for (int i = 0; i < paneles.Length; i++)
+        {
+            paneles[i].SetActive(i == indice);
+        }
+    }
+}
